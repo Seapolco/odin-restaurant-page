a5faac2fa304015b5bdc92d52681ca5b1d2027cb
Seapolco <seapolco@gmail.com>
2022-10-09 19:03:27 +0100

    First commit. Basic setup. Linking remote.
    Basic positioning and styling of first draft home page. About to develop the dynamic rendering.

---
+++ README.md
@@ -0,0 +1,1 @@
+Javascript generated restaurant page
---
+++ src/index.js
@@ -0,0 +1,12 @@
+console.log('Hungry?');
+
+// let hello = document.createElement('div');
+// let text = document.createTextNode('hello');
+
+
+
+// hello.appendChild(text);
+
+// hello.setAttribute('class','blue');
+
+// document.body.appendChild(hello);

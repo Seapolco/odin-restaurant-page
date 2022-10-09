d21d4425ea641d6d9a841dc4b3e7f06c0e832951
Seapolco <seapolco@gmail.com>
2022-10-09 19:52:51 +0100

    completed the logic to render the homepage dynamically.
    Now I need to move the logic from index.js to a seperate module.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,70 @@
+// import homepage from './home-page';
+
+
+// homepage();
+
 console.log('Hungry?');
+
+
+const elementFactory = function(type, attributes, ...children) {
+
+    const el = document.createElement(type);
+
+    if(attributes) {
+        for(key in attributes) {
+            el.setAttribute(key, attributes[key]);
+    
+        }
+    }
+
+
+
+    if(children) {
+        children.forEach(child => {
+            if (typeof child === 'string') {
+              el.appendChild(document.createTextNode(child))
+            } else {
+              el.appendChild(child)
+            }
+          })
+    }
+
+
+
+    return el 
+}
+
+let rootContainer = elementFactory('div', {class:'root-container'});
+
+let navBar = elementFactory(
+    'div', {class:'nav-bar'},
+        elementFactory(
+            'img',
+            {src: "../src//images/pngfind.com-grilled-chicken-breast-png-4604394.png",
+            class:"nav-logo"
+            }
+            ),
+        elementFactory('ul', {class:'nav-links'},
+            elementFactory('li',{}, 'Home'),
+            elementFactory('li',{}, 'Menu'),
+            elementFactory('li',{}, 'Contact')
+    )
+);
+
+let mainContent = elementFactory(
+    'div', 
+    {class:'main-content'},
+        elementFactory('div', {class:'hungry'}, 'Hungry?')
+);
+
+
+
+
+document.body.appendChild(rootContainer);
+rootContainer.appendChild(navBar);
+rootContainer.appendChild(mainContent);
+
+
 
 // let hello = document.createElement('div');
 // let text = document.createTextNode('hello');
